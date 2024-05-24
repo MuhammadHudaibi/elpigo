313bdaa6b935ea6c70fa9745fedc6ef3b5a87497
Dhea Puspita <dheapuspita363@gmail.com>
2024-05-24 15:36:22 +0800

    add splashscreen

---
+++ lib/app/modules/splash/views/splash_view.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../controllers/splash_controller.dart';
+
+class SplashView extends GetView<SplashController> {
+  const SplashView({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    Future.delayed(const Duration(seconds: 4), () {
+      Get.offNamed('/home');
+    });
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.asset(
+              'assets/logo.png',
+              height: 180,
+              width: 180,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

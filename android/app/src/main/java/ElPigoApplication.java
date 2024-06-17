21861f452971ed627b578c99da41f280edb34198
Muhammad Hudaibi <muhhudaibi11@gmail.com>
2024-06-17 15:08:51 +0700

    Menyimpan API Key di local

---
+++ android/app/src/main/java/ElPigoApplication.java
@@ -0,0 +1,10 @@
+package com.example.elpigo;
+
+import io.flutter.app.FlutterApplication;
+
+public class ElPigoApplication extends FlutterApplication {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
